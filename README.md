119acddca43f61d97cb73cf6f2efab17760f0e02
sjobo <s.bosma@tudelft.nl>
2015-03-05 22:28:47 +0100

    first version
    includes keyboard shortcut (ctrl+q), license(gpl), license attribution
    to other authors

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # qrify
 qrify plugin for chrome
+
+qrify uses qrcode.js developed by shim sangmin (https://davidshimjs.github.io/qrcodejs/) under MIT license. 
+
+qrify uses jquery developed by the jquery foundation (www.jquery.com) under MIT license.
---
+++ js/generateQR.js
@@ -0,0 +1,20 @@
+function getURL(){
+chrome.tabs.query({currentWindow: true, active: true}, function(tabs){displayURL(tabs);});
+}
+
+function displayURL(tabs)
+{
+	var current_url = tabs[0].url;
+	var qrcode = new QRCode("qrcode", {
+    text: current_url,
+    width: 250,
+    height: 250,
+    colorDark : "#000000",
+    colorLight : "#ffffff",
+    correctLevel : QRCode.CorrectLevel.H
+	});
+}
+
+document.addEventListener('DOMContentLoaded', function(){getURL();});
+chrome.browserAction.setBadgeText({text:"beta"});
+chrome.browserAction.setBadgeBackgroundColor({color:"#000000"});
